df97e6108834242eb6bfea3cb6ee122a5e125da9
TynysA <jjakellucas@gmail.com>
2023-02-12 22:56:10 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/fl1_prj1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fl1_prj1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle xoStyle =
+TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0, color: Colors.blue);
+
+const TextStyle txtStyle = TextStyle(
+    fontWeight: FontWeight.bold, fontSize: 26.0, color: Colors.blueAccent);
